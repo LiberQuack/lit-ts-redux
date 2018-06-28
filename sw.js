b25cefc5a99b189ca25758f31f7477cc7a246bb5
Thiago Martins <rogue.thiago@gmail.com>
2018-06-28 14:10:21 -0300

    Updates

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-importScripts("/lit-ts-redux/precache-manifest.c859b2726c927fb920fcfa77b44fa0fc.js", "/lit-ts-redux/workbox-v3.3.0/workbox-sw.js");
+importScripts("/lit-ts-redux/precache-manifest.56e62f2bcc9a74894e40cc6b0f6c5402.js", "/lit-ts-redux/workbox-v3.3.0/workbox-sw.js");
 workbox.setConfig({modulePathPrefix: "/lit-ts-redux/workbox-v3.3.0"});
 //Wepback will import workbox and preachemanifest here;
 workbox.precaching.precacheAndRoute(self.__precacheManifest || []);
